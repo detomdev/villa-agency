80058c9eeada8d3f89791e2a6af8eb5e16f574af
Richard DeToma <rdetoma@yahoo.com>
2024-03-21 11:49:16 -0400

    Adding media queries

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
+const nav = document.querySelector(".nav--primary");
 
 navToggle.addEventListener("click", () => {
 	nav.classList.toggle("nav--visible");
