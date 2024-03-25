103d4b12578614bc170e9367c37e70ba443e37cf
Richard DeToma <rdetoma@yahoo.com>
2024-03-25 13:36:00 -0400

    Adjustinf feature section and video section media queries

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav--primary");
+const nav = document.querySelector(".nav");
 
 navToggle.addEventListener("click", () => {
 	nav.classList.toggle("nav--visible");
