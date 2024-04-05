17475054c8e8834d291f43e29687d1bae641d5a9
Richard DeToma <rdetoma@yahoo.com>
2024-04-05 15:34:55 -0400

    Changed the feature button section to FAQ

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
+"use strict";
+
+// Mobile navigation
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector(".nav");
 
 navToggle.addEventListener("click", () => {
 	nav.classList.toggle("nav--visible");
+});
+
+//FAQ drop down
+//Bring in all the buttons. This puts them in a node list (array)
+const toggles = document.querySelectorAll(".faq-toggle");
+
+//use forEach to loop over the individual 'toggle' and add the event listener "click" the callback function will go to the parent and toggle the class to active
+toggles.forEach(toggle => {
+	toggle.addEventListener("click", () => {
+		toggle.parentNode.classList.toggle("active");
+	});
 });
