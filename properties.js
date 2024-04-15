2736003be9e8a5550cb1272706e2c869b2e30347
Richard DeToma <rdetoma@yahoo.com>
2024-04-15 14:03:34 -0400

    Button script set

--- properties.js
+++ properties.js
@@ -1,11 +1,12 @@
-//FAQ drop down
-//Bring in all the buttons. This puts them in a node list (array)
-const toggles = document.querySelectorAll(".toggle");
-console.log(toggles);
+// Get all the buttons
+const buttons = document.querySelectorAll(".btn__general");
 
-//use forEach to loop over the individual 'toggle' and add the event listener "click" the callback function will change the button color
-toggles.forEach(toggle => {
-	toggle.addEventListener("click", () => {
-		toggle.style.backgroundColor = "var(--clr-light)";
+buttons.forEach(button => {
+	button.addEventListener("click", () => {
+		// Remove 'active' class from all buttons
+		buttons.forEach(btn => btn.classList.remove("active"));
+
+		// Add 'active' class to the clicked button
+		button.classList.add("active");
 	});
 });
